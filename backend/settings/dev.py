b25ecb5b74ca4d251dc1752f4da6651f37502a17
Renujan <roshanrenujan20020426@gmail.com>
2026-06-01 11:01:16 +0530

    feat: configure Django project settings (base, dev, production)

---
+++ backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-e4y9b=67i@ic%hy1-@_wnaabbf(0-n0w++c5n#u10#05!*c4h%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
