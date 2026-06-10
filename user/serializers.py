10886d5032acc57a5ff0ec4403ecb562dfa9fc08
Renujan <roshanrenujan20020426@gmail.com>
2026-06-10 10:11:03 +0530

    feat(api): define BusinessTypeSerializer and update BusinessProfileSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import (
-    Niche, BusinessProfile, CreatorProfile, CreatorRate, CreatorSocialAccount
+    Niche, BusinessType, BusinessProfile, CreatorProfile, CreatorRate, CreatorSocialAccount
 )
 
 class NicheSerializer(serializers.ModelSerializer):
     class Meta:
         model = Niche
+        fields = ["id", "name"]
+
+class BusinessTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessType
         fields = ["id", "name"]
 
 class CreatorRateSerializer(serializers.ModelSerializer):
@@ -26,11 +31,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class BusinessProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    business_types = BusinessTypeSerializer(many=True, read_only=True)
 
     class Meta:
         model = BusinessProfile
         fields = [
-            "id", "user", "company_name", "business_type", "website", "bio",
+            "id", "user", "company_name", "business_type", "business_types", "website", "bio",
             "phone", "secondary_phone", "time_zone", "avatar_url",
             "facebook_url", "instagram_handle", "tiktok_handle", "youtube_url",
             "linkedin_url", "twitter_handle", "otp_verified", "status"
